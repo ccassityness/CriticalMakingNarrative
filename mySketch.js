b94da585ee16dd0f35453ea10c2eba154c1b69d3
Cailey Cassity-Ness <ca329938@ucf.edu>
2026-05-22 15:41:22 -0400

    Refactor hex generation and particle drawing

--- mySketch.js
+++ mySketch.js
@@ -1,81 +1,95 @@
-//Let's combine the two previous demos
-//Now we're going to let the lines move after we create them
-//Try changing the lines as before
-lines=["touch the screen","set the tone","make it work","craft the computation","code the thing","connect the dots","interact with the machine","push the text","pull the code","press on"]
-//We will use i to track which line we just showed
-i=0;
-//But we'll also create a box to hold our lines as they move
+lines = ["touch the screen","set the tone","make it work","craft the computation","code the thing","connect the dots","interact with the machine","push the text","pull the code","press on"]
+i = 0;
 particles = [];
 
+let hexes = [];
+const R = 36;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-//Try changing the initial background color
-	background(50);
+  createCanvas(windowWidth, windowHeight);
+  generateHexes();
 }
+
+function generateHexes() {
+  hexes = [];
+  const hexW = R * sqrt(3);
+  const vertSpacing = R * 1.5;
+  let row = 0;
+  for (let y = R; y < height + R; y += vertSpacing, row++) {
+    const offset = (row % 2) * (hexW / 2);
+    for (let x = hexW / 2 + offset; x < width + hexW; x += hexW) {
+      hexes.push({
+        x, y,
+        col: color(random(200, 255), random(150, 220), random(0, 60))
+      });
+    }
+  }
+}
+
+function drawHex(cx, cy, r) {
+  beginShape();
+  for (let i = 0; i < 6; i++) {
+    const angle = radians(60 * i - 30);
+    vertex(cx + r * cos(angle), cy + r * sin(angle));
+  }
+  endShape(CLOSE);
+}
+
 function draw() {
-	background(50);
-	//This moves the particles
+  // Draw yellow honeycomb background
+  noStroke();
+  for (let h of hexes) {
+    fill(h.col);
+    drawHex(h.x, h.y, R - 1);
+  }
+
+  // Draw particles on top
   for (let i = particles.length - 1; i >= 0; i--) {
     particles[i].update();
     particles[i].show();
     if (particles[i].finished()) {
-      // remove this particle
       particles.splice(i, 1);
     }
   }
 }
-//This draws the word with each mouse click
-function mouseClicked() {
-	let p = new Particle(mouseX,mouseY,lines[i]);
-    particles.push(p);
-	if (i<(lines.length-1)) {
-		i++;
-	}
-	else {
-		i=0;
-	}
 
+function mouseClicked() {
+  let p = new Particle(mouseX, mouseY, lines[i]);
+  particles.push(p);
+  if (i < (lines.length - 1)) {
+    i++;
+  } else {
+    i = 0;
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  generateHexes();
 }
 
 class Particle {
-  constructor(x,y,text) {
-		//This sets the x value to mouse position
+  constructor(x, y, text) {
     this.x = x;
-		//This keeps the y at mouse position
     this.y = y;
-		//This sets the range of x movement - try limiting it to + or -
     this.vx = random(-1, 1);
-		//This sets the range of y movement - try limiting it to + or -
     this.vy = random(-1, 1);
-		//This sets the text size to be consistent
-		this.size = random(15,50);
-		//This sets the current line to the particle
-		this.text = text;
+    this.size = random(15, 50);
+    this.text = text;
   }
-
   finished() {
     return (this.x < 0 || this.x > windowWidth);
   }
-
   update() {
     this.x += this.vx;
     this.y += this.vy;
   }
-
   show() {
     noStroke();
-		textSize(this.size);
-		//Try any web safe font
-		textFont("Courier");
-		//This centers the text on the click
-		textAlign(CENTER, CENTER);
-		//This sets the fill to a static color - can you make it random?
-		//You can also add the outline
-    //stroke(255);
-		//This keeps R and G values at 255 to allow for yellows
-		//Try changing it!
+    textSize(this.size);
+    textFont("Courier");
+    textAlign(CENTER, CENTER);
     fill("green");
-		//This positions the text
     text(this.text, this.x, this.y);
   }
 }
